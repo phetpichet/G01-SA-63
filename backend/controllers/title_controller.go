4d4e495a6c47438c64d6d083eca17114df69c7d3
phetpichet <70629344+phetpichet@users.noreply.github.com>
2020-11-22 12:13:56 +0700

    Update title_controller.go

--- backend/controllers/title_controller.go
+++ backend/controllers/title_controller.go
@@ -3,8 +3,8 @@ package controllers
 import (
 	"context"
 	"strconv"
-	"github.com/Piichet-3-/app/ent"
-	"github.com/Piichet-3-/app/ent/title"
+	"github.com/Piichet/app/ent"
+	"github.com/Piichet/app/ent/title"
 	"github.com/gin-gonic/gin"
 )
 
